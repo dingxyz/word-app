210131062aeb9a5b053b11ee56f14c161106aabe
dingsaili <dingsaili@hzwtech.com>
2024-11-26 13:27:10 +0800

    feat|Style optimization

--- front-end/src/views/home/word-item/index.vue
+++ front-end/src/views/home/word-item/index.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import WordApi, {IWord} from '@/api/word-api'
-import {computed, defineComponent, ref, watch} from 'vue'
-import IconBtn from "@/components/IconBtn.vue";
-import {useAppStore} from "@/stores/useApp";
-import {showConfirmDialog, showNotify} from "vant";
-import {copyToClipboard} from "@/utils/common-util";
-import {marked} from "marked";
-import 'github-markdown-css/github-markdown.css';
-import {ORDER_TYPE, useVoiceStore} from "@/stores/useVoice";
+import WordApi, { IWord } from '@/api/word-api'
+import { computed, defineComponent, ref, watch } from 'vue'
+import IconBtn from '@/components/IconBtn.vue'
+import { useAppStore } from '@/stores/useApp'
+import { showConfirmDialog, showNotify } from 'vant'
+import { copyToClipboard } from '@/utils/common-util'
+import { marked } from 'marked'
+import 'github-markdown-css/github-markdown.css'
+import { ORDER_TYPE, useVoiceStore } from '@/stores/useVoice'
 
 defineComponent({
-  name: 'WordItem',
+  name: 'WordItem'
 })
 
 const poops = defineProps<{
@@ -24,28 +24,36 @@ const wordItemRef = ref()
 const emit = defineEmits(['refresh-list', 'edit-word'])
 const showDetailPopup = ref(false)
 const showChinese = ref(appStore.showChineseChecked)
-const compiledMarkdown = computed(() => `<h3>${poops.wordData.english}</h3>`+marked(poops.wordData.annotation));
+const compiledMarkdown = computed(
+  () => `<h3>${poops.wordData.english}</h3>` + marked(poops.wordData.annotation)
+)
 const isPlaying = computed(() => !voiceStore.isPaused && voiceStore.playingId === poops.wordData.id)
 const isPlayingByClick = ref(false) // Manual click play
 
 const observer = new IntersectionObserver((entries) => {
-  const entry = entries[0];
+  const entry = entries[0]
   if (!entry.isIntersecting || appStore.isLiteMode) {
     wordItemRef.value?.scrollIntoView({
       block: voiceStore.playOrder === ORDER_TYPE.SEQUENTIAL ? 'start' : 'center'
-    });
-    observer.disconnect();
-  }
-});
-
-watch(() => voiceStore.playingId, () => {
-  if (isPlaying.value && wordItemRef.value) {
-    observer.observe(wordItemRef.value);
-  } else {
-    observer?.disconnect();
+    })
+    observer.disconnect()
   }
 })
-watch(() => appStore.showChineseChecked, (val) => showChinese.value = val)
+
+watch(
+  () => voiceStore.playingId,
+  () => {
+    if (isPlaying.value && wordItemRef.value) {
+      observer.observe(wordItemRef.value)
+    } else {
+      observer?.disconnect()
+    }
+  }
+)
+watch(
+  () => appStore.showChineseChecked,
+  (val) => (showChinese.value = val)
+)
 
 const removeHandler = async (id: string) => {
   const confirm = await showConfirmDialog({
@@ -55,11 +63,11 @@ const removeHandler = async (id: string) => {
     closeOnClickOverlay: true,
     cancelButtonText: 'Cancel',
     cancelButtonColor: '#c7c7c7',
-    confirmButtonText: 'Confirm',
+    confirmButtonText: 'Confirm'
   }).catch(() => false)
   if (!confirm) return
   await WordApi.remove(id, {
-    wordType: appStore.wordType,
+    wordType: appStore.wordType
   })
   emit('refresh-list')
 }
@@ -68,16 +76,16 @@ const moveToLearned = async () => {
   await WordApi.moveTo({
     id: poops.wordData.id,
     wordType: poops.wordData.wordType,
-    toType: 'learned',
+    toType: 'learned'
   }).then(() => {
-    showNotify({type: 'success', message: 'Moved to learned successfully'});
+    showNotify({ type: 'success', message: 'Moved to learned successfully' })
   })
   emit('refresh-list')
 }
 
-let timeoutId = null;
+let timeoutId = null
 const mouseHandler = (isClick?: boolean) => {
-  showChinese.value = !showChinese.value;
+  showChinese.value = !showChinese.value
   if (!poops.wordData.chinese) {
     showChinese.value = false
   }
@@ -94,7 +102,7 @@ const mouseHandler = (isClick?: boolean) => {
   }
 }
 
-let longPressTimer = null;
+let longPressTimer = null
 const startLongPress = () => {
   longPressTimer = setTimeout(() => {
     copyToClipboard(showChinese.value ? poops.wordData.chinese : poops.wordData.english)
@@ -111,12 +119,12 @@ const endLongPress = () => {
 const detailPopupClickHandler = (event: MouseEvent) => {
   if (event.target instanceof HTMLElement) {
     const tagName = event.target.tagName.toLowerCase()
-    if (tagName ==='strong') {
+    if (tagName === 'strong') {
       const oldColor = (event.target as HTMLElement).style.color
       event.target.style.color = '#02c3ff'
       const text = event.target.textContent
       voiceStore.voiceSpeak(text, false, () => {
-        (event.target as HTMLElement).style.color = oldColor
+        ;(event.target as HTMLElement).style.color = oldColor
       })
     }
   }
@@ -129,24 +137,41 @@ const playSound = (english: string) => {
   })
 }
 
-const openDetail = () => showDetailPopup.value = true
-
+const openDetail = () => (showDetailPopup.value = true)
 </script>
 
 <template>
-  <div ref="wordItemRef" class="text-[#fff] text-lg bg-[#935211] odd:bg-[#83480c]" :class="{'!text-blue-300': isPlaying || isPlayingByClick }">
+  <div
+    ref="wordItemRef"
+    class="text-[#fff] text-lg bg-[#935211] odd:bg-[#83480c]"
+    :class="{ '!text-blue-300': isPlaying || isPlayingByClick }"
+  >
     <van-swipe-cell>
-      <li class="flex items-center h-16">
+      <li class="flex items-center">
         <div
-          class="word-box self-start flex-1 leading-tight pl-2"
-          :class="{'show-chinese': showChinese}"
+          class="word-box self-start flex-1 leading-normal pl-2"
+          :class="{ 'show-chinese': showChinese }"
           @click="playSound(wordData.english)"
         >
-          <div @touchstart="startLongPress" @touchmove="endLongPress" @touchend="endLongPress" class="btn h-16 flex items-center">{{ wordData.english }}</div>
-          <div @touchstart="startLongPress" @touchmove="endLongPress" @touchend="endLongPress" class="btn h-16 flex items-center text-slate-400 text-base">{{ wordData.chinese ?? '--' }}</div>
+          <div
+            @touchstart="startLongPress"
+            @touchmove="endLongPress"
+            @touchend="endLongPress"
+            class="first-text min-h-16 btn h-auto py-2 flex items-center"
+          >
+            {{ wordData.english }}
+          </div>
+          <div
+            @touchstart="startLongPress"
+            @touchmove="endLongPress"
+            @touchend="endLongPress"
+            class="second-text min-h-16 btn h-auto py-2 flex items-center text-slate-400 text-base"
+          >
+            {{ wordData.chinese ?? '--' }}
+          </div>
         </div>
-        <IconBtn v-if="wordData.annotation" icon="eye-o" @click="openDetail"/>
-        <IconBtn icon="exchange" v-if="wordData.chinese" @click="mouseHandler(true)"/>
+        <IconBtn v-if="wordData.annotation" icon="eye-o" @click="openDetail" />
+        <IconBtn icon="exchange" v-if="wordData.chinese" @click="mouseHandler(true)" />
       </li>
       <template #left>
         <div class="p-1 text-sm text-fuchsia-500">
@@ -155,16 +180,30 @@ const openDetail = () => showDetailPopup.value = true
       </template>
       <template #right>
         <div class="flex items-center">
-          <IconBtn icon="delete-o" @click="removeHandler(wordData.id)" color="red"/>
-          <IconBtn icon="edit" @click="emit('edit-word', wordData)"/>
+          <IconBtn icon="delete-o" @click="removeHandler(wordData.id)" color="red" />
+          <IconBtn icon="edit" @click="emit('edit-word', wordData)" />
         </div>
       </template>
     </van-swipe-cell>
-    <van-popup v-model:show="showDetailPopup" v-if="wordData.annotation" closeable lazy-render class="flex p-4 bg-black" position="bottom">
+    <van-popup
+      v-model:show="showDetailPopup"
+      v-if="wordData.annotation"
+      closeable
+      lazy-render
+      class="flex p-4 bg-black"
+      position="bottom"
+    >
       <div class="flex-1 flex flex-col">
-        <div v-html="compiledMarkdown" v-bold-english @click="detailPopupClickHandler" class="markdown-body flex-auto overflow-auto bg-black text-white"></div>
+        <div
+          v-html="compiledMarkdown"
+          v-bold-english
+          @click="detailPopupClickHandler"
+          class="markdown-body flex-auto overflow-auto bg-black text-white"
+        ></div>
         <div class="text-center mt-4">
-          <van-button type="success" @click="showDetailPopup = false" class="w-full">CLOSE</van-button>
+          <van-button type="success" @click="showDetailPopup = false" class="w-full"
+            >CLOSE
+          </van-button>
         </div>
       </div>
     </van-popup>
@@ -173,15 +212,22 @@ const openDetail = () => showDetailPopup.value = true
 
 <style scoped lang="scss">
 .word-box {
-  transform: translateY(0);
-  transition: transform 0.3s ease;
-
+  .second-text {
+    display: none;
+  }
   &.show-chinese {
-    transform: translateY(-50%);
+    .first-text {
+      display: none;
+    }
+
+    .second-text {
+      display: flex;
+    }
   }
 }
 
-:deep(.van-swipe-cell__right), :deep(.van-swipe-cell__left) {
+:deep(.van-swipe-cell__right),
+:deep(.van-swipe-cell__left) {
   display: flex;
   align-items: center;
 }
